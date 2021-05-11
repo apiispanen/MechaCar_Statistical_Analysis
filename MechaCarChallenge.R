4601f494506906c1cca3fbecff0d4ef2cc3a6713
Andrew Piispanen <apiispanen1@babson.edu>
2021-05-11 00:07:44 -0400

    T Tests overall and by lot

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,4 +11,12 @@ summary(suspension_coil)
 
 total_summary <- suspension_coil %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
 
-lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create lot summary table
+
+# T Tests: Overall
+t.test(suspension_coil$PSI,mu=1500) #compare sample versus population means
+
+# T Tests: Subsets by lot
+t.test(subset(suspension_coil[suspension_coil$Manufacturing_Lot == "Lot1",])$PSI,mu=1500) #compare lot1 versus population means
+t.test(subset(suspension_coil[suspension_coil$Manufacturing_Lot == "Lot2",])$PSI,mu=1500) #compare lot2 versus population means
+t.test(subset(suspension_coil[suspension_coil$Manufacturing_Lot == "Lot3",])$PSI,mu=1500) #compare lot3 versus population means
